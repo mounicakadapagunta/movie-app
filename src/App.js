c46e098df58c070a7e45782a113868b573884d30
mounicakadaagunta <mounicakadapagunta13@gmail.com>
2021-11-02 12:08:37 +0530

    movie name changes

--- src/App.js
+++ src/App.js
@@ -3,16 +3,9 @@
 export default function App() {
   // const names =["ajith", "kewal","Ryed","Deepa","Robin"]
   const movies = [
+  
     {
-      name: "1. Nenokkadine",
-      pic: "https://i.pinimg.com/736x/fe/f2/b5/fef2b5d2edad7c05b9ca2d98a97a2868--mahesh-babu-movie.jpg",
-      rating: 8.2,
-      summary: "A rock star must overcome his psychological inhibitions to seek revenge for his parents' death.",
-      director: "Sukumar"
-    },
-
-    {
-      name: "2. Athadu",
+      name: "1. Athadu",
       pic: "https://www.filmibeat.com/ph-big/2011/09/1317193759547588.jpg",
       rating: 8.2,
       summary: "A gunman for hire is framed for murder, and assumes a dead man's identity while hiding from the police.",
@@ -20,7 +13,7 @@ export default function App() {
     },
 
     {
-      name: "3. Okkadu",
+      name: "2. Okkadu",
       pic: "https://pbs.twimg.com/media/EP1GKeDU8AAHJ9A.jpg",
       rating: 8.0,
       summary: "A Kabbadi player rescues a young woman from an unwanted marriage and hides her in his home.",
@@ -28,7 +21,7 @@ export default function App() {
     },
 
     {
-      name: "4. Pokiri",
+      name: "3. Pokiri",
       pic: "https://m.media-amazon.com/images/M/MV5BZjFiMDljZmItZjE1Ny00ZDAyLTlkNDktNmExYTc1YmJhYmJmXkEyXkFqcGdeQXVyNDY5MTUyNjU@._V1_.jpg",
       rating: 7.9,
       summary: "A local goon's killer instincts earn him his girlfriend's disapproval, a corrupt cop's enmity and a wanted don's attention.",
@@ -36,7 +29,7 @@ export default function App() {
     },
 
     {
-      name: "5. Srimanthudu",
+      name: "4. Srimanthudu",
       pic: "https://i.pinimg.com/originals/15/b9/4b/15b94bd7ac26e79a98b17939cbe3c0f5.jpg",
       rating: 7.6,
       summary: "Harsha, a multi-millionaire who has everything, still feels that there is something missing in his life. In an attempt to fill the void, he adopts a village to bring change in the people.",
@@ -44,7 +37,7 @@ export default function App() {
     },
 
     {
-      name: "6. Khaleja ",
+      name: "5. Khaleja ",
       pic: "https://64.media.tumblr.com/11eb5ea6cb56c417b4706511191d12e1/tumblr_nfnoau8gIL1r9esjeo1_1280.jpg",
       rating: 7.6,
       summary: "When a mysterious illness ravages a remote village, the villagers embrace a reluctant taxi driver as their savior.",
@@ -52,7 +45,7 @@ export default function App() {
     },
 
     {
-      name: "7. Dookudu",
+      name: "6. Dookudu",
       pic: "https://wallpaperaccess.com/full/2490926.jpg",
       rating: 7.4,
       summary: "Ajay, (Mahesh Babu) an undercover cop, is assigned to catch a dangerous mafia don with whom he has a personal score to settle.",
@@ -60,7 +53,7 @@ export default function App() {
     },
 
     {
-      name: "8. Spyder",
+      name: "7. Spyder",
       pic: "https://moviegalleri.net/wp-content/gallery/mahesh-babu-spyder-movie-release-tomorrow-posters/mahesh-babu-spyder-movie-release-tomorrow-posters-382fb76.jpg",
       rating: 6.6,
       summary: "An intelligence officer attempts to save the city from a psychotic serial killer who kills people just to hear their loved ones cry.",
@@ -68,7 +61,7 @@ export default function App() {
     },
 
     {
-      name: "9. Businessman",
+      name: "8. Businessman",
       pic: "https://i.pinimg.com/474x/50/90/14/509014d1dbbb64c764aeb951327941c1.jpg",
       rating: 7.2,
       summary: "Surya arrives in Mumbai to revive the mafia and ends up making enemies who threaten his life.",
