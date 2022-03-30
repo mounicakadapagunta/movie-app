77a308c5542e3f5d501e714f7be70f74225f4407
mounicakadaagunta <mounicakadapagunta13@gmail.com>
2022-03-30 19:20:52 +0530

    form code updated

---
+++ src/BasicForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function () {
+    return (
+        <div>
+            <input type="email" placeholder="Enter your Email" />
+            <input type="password" placeholder="Enter your password" />
+            <button>Submit</button>
+
+        </div>
+    )
+}
